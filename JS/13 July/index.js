a43f213b54c488e527910ee751511b65aa12b51f
Hrishikesh <nalawadehrishi35@gmail.com>
2023-07-14 10:22:57 +0530

    second lecture on javascript

---
+++ JS/13 July/index.js
@@ -0,0 +1,21 @@
+// let soul;
+// soul = 44;
+// soul = 55;
+// console.log(soul);
+
+const a = 44;
+console.log(a)
+
+console.log(typeof a);
+
+
+var hrishi = 23.11;
+console.log(hrishi);
+console.log(typeof hrishi);
+
+
+var name = "hrishi";
+console.log(name);
+console.log(typeof name);
+console.log("Hello I am " + name + ". I am from " + institution);
+console.log(`hello i am ${name} I am from ${institution}`);
